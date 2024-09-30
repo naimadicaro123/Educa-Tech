3584ccac5c6e68702644dcbb5790646948069d53
Icaro Damian <icarodamian612@gmai.com>
2024-09-30 12:51:36 -0300

    Adicionei os dados de saude

---
+++ matri3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados de Saúde</title>
+    <link rel="stylesheet" href="css's/style.css">
+    <link rel="icon" href="imagens/favicon.svg" type="image/svg+xml">
+<link rel="icon" href="imagens/favicon.png" type="image/png">
+<link rel="icon" href="imagens/favicon.ico" type="image/x-icon">
+</head>
+<body>
+<?php
+   include('layouts/header.php');
+    ?>
+    <main>
+<section class="centrelized">
+    <h1 class></h1>
+<form id="health">
+        
+    </form>
+</section>
+</main>    
+</body>
+</html>
